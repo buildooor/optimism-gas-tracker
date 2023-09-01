84ab21645c3b3fabcfb14b5be50a97b7c3041ccc
buildooor <buildooor@protonmail.com>
2023-09-01 16:38:04 -0700

    frontend: update meta

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Optimism Gas Tracker',
-  description: 'Optimism Gas Tracker site for tracking gas prices on Optimism Mainnet network.',
+  description: 'Optimism Gas Tracker site for tracking gas prices and top gas spenders on Optimism Mainnet network.',
 }
 
 export default function RootLayout({
@@ -18,7 +18,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
+        <meta charSet="UTF-8" />
+        <meta http-equiv="content-language" content="en-us" />
+        <meta name="description" content="Optimism Gas Tracker site for tracking gas prices and top gas spenders on Optimism Mainnet network." />
+        <meta name="keywords" content="optimism, gas, tracker, etherscan, spenders, guzzlers, prices, historical, history, table, chart" />
+        <meta name="robots" content="index,follow" />
+        <meta name="googlebot" content="index,follow" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="application-name" content="Optimism Gas Tracker" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="Optimism Gas Tracker" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="author" content="Buildooor" />
+        <link rel="canonical" href="https://optimismgastracker.com" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
