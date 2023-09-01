3af4f77d93f5c9f8e4931afccdeec7e91636667d
buildooor <buildooor@protonmail.com>
2023-08-31 22:20:26 -0700

    Debug cache

--- backend/src/responseCache.ts
+++ backend/src/responseCache.ts
@@ -1,6 +1,6 @@
 import mcache from 'memory-cache'
 
-const durationMs = 5 * 1000
+const durationMs = 120 * 1000
 
 export function responseCache (req: any, res: any, next: any) {
   const urlKey = req.originalUrl || req.url
