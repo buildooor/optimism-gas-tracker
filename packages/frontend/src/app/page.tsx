02d0f7e716d799bf62c73f5828a328d2794002b6
buildooor <buildooor@protonmail.com>
2023-09-01 17:09:05 -0700

    frontend: dry fetching

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -8,10 +8,198 @@ import { useQuery } from 'react-query'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import Skeleton from '@mui/material/Skeleton'
 
-const refreshInterval = 10 * 1000
+const fastRefreshInterval = 10 * 1000
+const slowRefreshInterval = 60 * 1000
 const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'
 
-const Countdown = ({ initialCount = 10 }) => {
+async function fetchData (method: string, params: any[] = []) {
+  return fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      method,
+      params,
+      id: 1
+    })
+  })
+  .then(response => response.json())
+}
+
+function useDate() {
+  const { isLoading, data, error } = useQuery(['date'], async () => {
+    const dt = DateTime.utc()
+    const formattedDate = dt.toFormat('ccc, dd LLL yyyy HH:mm:ss \'UTC\'')
+    return formattedDate
+  }, {
+    enabled: true,
+    refetchInterval: 1 * 1000
+  })
+
+  return {
+    date: data ?? ''
+  }
+}
+
+function useRelativeDate(timestamp: number) {
+  const { isLoading, data, error } = useQuery([`relativedate:${timestamp}`, timestamp], async () => {
+    if (!timestamp) {
+      return ''
+    }
+    return DateTime.fromSeconds(timestamp).toRelative()
+  }, {
+    enabled: true,
+    refetchInterval: 1 * 1000
+  })
+
+  return {
+    date: data ?? ''
+  }
+}
+
+function useGetCurrentGasPrice() {
+  const { isLoading, data, error } = useQuery(['currentGasPrice'], async () => {
+    return fetchData('gasTracker_getCurrentGasPrice', [])
+    .then((data: any) => data.result)
+  }, {
+    enabled: true,
+    refetchInterval: fastRefreshInterval
+  })
+
+  return {
+    gasPrice: data?.gasPrice,
+    timestamp: data?.timestamp,
+  }
+}
+
+function useGetCurrentEthUsdPrice() {
+  const { isLoading, data, error } = useQuery(['currentEthUsdPrice'], async () => {
+    return fetchData('gasTracker_getCurrentEthUsdPrice', [])
+    .then((data: any) => data.result)
+  }, {
+    enabled: true,
+    refetchInterval: fastRefreshInterval
+  })
+
+  return {
+    price: data?.price,
+    priceDisplay: data?.priceDisplay,
+    timestamp: data?.timestamp,
+  }
+}
+
+function useGasEstimates() {
+  const { isLoading, data, error } = useQuery(['gasEstimates'], async () => {
+    return fetchData('gasTracker_getGasEstimates', [])
+    .then((data: any) => data.result.estimates)
+  }, {
+    enabled: true,
+    refetchInterval: fastRefreshInterval
+  })
+
+  return {
+    estimates: data ?? []
+  }
+}
+
+function useTimeRange(defaultValue: string) {
+  const [timeRange, setTimeRange] = useState(defaultValue)
+  return {
+    timeRange,
+    setTimeRange
+  }
+}
+
+function useGasPrices(timeRange: string) {
+  const { isLoading, data, error } = useQuery(['gasPrices', timeRange], async () => {
+    return fetchData('gasTracker_getHistoricalGasPrices', [timeRange])
+    .then((data: any) => data.result.gasPrices)
+  }, {
+    enabled: true,
+    refetchInterval: fastRefreshInterval
+  })
+
+  return {
+    gasPrices: data ?? []
+  }
+}
+
+function useTopGasSpenders (timeRange: string) {
+  const { isLoading, data, error } = useQuery(['topGasSpenders', [timeRange]], async () => {
+    return fetchData('gasTracker_getTopGasSpenders', [timeRange])
+    .then((data: any) => data.result.gasSpenders)
+  }, {
+    enabled: true,
+    refetchInterval: slowRefreshInterval
+  })
+
+  return {
+    topGasSpenders: data ?? []
+  }
+}
+
+function useTopGasGuzzlers(timeRange: string) {
+  const { isLoading, data, error } = useQuery(['topGasGuzzlers', timeRange], async () => {
+    return fetchData('gasTracker_getTopGasGuzzlers', [timeRange])
+    .then((data: any) => data.result.gasGuzzlers)
+  }, {
+    enabled: true,
+    refetchInterval: slowRefreshInterval
+  })
+
+  return {
+    topGasGuzzlers: data ?? []
+  }
+}
+
+function Banner () {
+  return (
+    <div className={styles.banner}>
+      <div className={styles.innerBanner}>
+        {'⚠️ This project is still in development.'}
+      </div>
+    </div>
+  )
+}
+
+function Header () {
+  return (
+    <div className={styles.titleContainer}>
+      <a href="/"><Image src="/assets/optimism.svg" alt="" width={200} height={40} /> <span className={styles.title}>Gas Tracker ⛽</span></a>
+    </div>
+  )
+}
+
+function SubHeader (props: any) {
+  const { blockInfo } = props
+  const { date } = useDate()
+  return (
+    <div className={styles.subTitleContainer}>
+      {/*<Countdown initialCount={refreshInterval / 1000} />*/}
+      <BlockInfo blockNumber={blockInfo?.blockNumber} gasPrice={blockInfo?.gasPrice} />
+      <div title="Current date and time in UTC">{date}</div>
+    </div>
+  )
+}
+
+function BlockInfo (props: any) {
+  const { blockNumber, gasPrice } = props
+  return (
+    <div className={styles.blockInfo}>
+      {!!gasPrice?.gwei && (
+        <div className={styles.blockInfoGasPrice} title="Latest gas price denominated in Gwei">Gas Price: {gasPrice?.gwei} gwei</div>
+      )}
+      {!!blockNumber && (
+        <div className={styles.blockInfoBlock} title="Block number of gas price shown">
+          <a href={`https://optimistic.etherscan.io/block/${blockNumber}`} target="_blank">Last updated block {blockNumber}</a>
+        </div>
+      )}
+    </div>
+  )
+}
+
+function Countdown ({ initialCount = 10 }) {
   const [count, setCount] = useState(initialCount)
 
   useEffect(() => {
@@ -36,7 +224,65 @@ const Countdown = ({ initialCount = 10 }) => {
   )
 }
 
-const GasTable = (props: any) => {
+function TimeRange (props: any) {
+  const { onChange, timeRange } = props
+  let readableTimeRange = ''
+  if (timeRange === '10m') {
+    readableTimeRange = '10 minutes'
+  }
+  if (timeRange === '1h') {
+    readableTimeRange = 'hour'
+  }
+  if (timeRange === '24h') {
+    readableTimeRange = '24 hours'
+  }
+  if (timeRange === '7d') {
+    readableTimeRange = '7 days'
+  }
+  return (
+    <div className={styles.timeRangeContainer}>
+      <div title="Time range for data shown">Last {readableTimeRange}</div>
+      <div className={styles.timeRange}>
+        <button title="10 minutes" className={timeRange === '10m' ? styles.timeRangeButtonSelected : styles.timeRangeButton} onClick={() => onChange('10m')}>10m</button>
+        <button title="1 hour" className={timeRange === '1h' ? styles.timeRangeButtonSelected : styles.timeRangeButton} onClick={() => onChange('1h')}>1H</button>
+        <button disabled title="24 hours (NOT ENOUGH DATA YET)" className={timeRange === '24h' ? styles.timeRangeButtonSelected : styles.timeRangeButton} onClick={() => onChange('24h')}>24H</button>
+        <button disabled title="7 days (NOT ENOUGH DATA YET)" className={timeRange === '7d' ? styles.timeRangeButtonSelected : styles.timeRangeButton} onClick={() => onChange('7d')}>7D</button>
+      </div>
+    </div>
+  )
+}
+
+function TableTabs (props: any) {
+  const { activeTab, setActiveTab } = props
+
+  return (
+    <div className={styles.tableTabsContainer}>
+      <div className={styles.tableTabs}>
+        <div onClick={event => setActiveTab('tab1')}>
+          <TableTab selected={activeTab === 'tab1'} title="Top Gas Guzzlers" subtitle="Sending Accounts that pay a lot of Gas" />
+        </div>
+        <div onClick={event => setActiveTab('tab2')}>
+          <TableTab selected={activeTab === 'tab2'} title="Top Gas Spenders" subtitle="Contracts / Accounts that consume a lot of Gas" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function TableTab(props: any) {
+  const { title, subtitle, blockNumber, selected } = props
+  return (
+    <div className={selected ? styles.tableTabSelected : styles.tableTab}>
+      <h2 className={styles.tableTabTitle}>⛽ {title}</h2>
+      <div className={styles.tableTabSubtitle}>{subtitle}</div>
+      {blockNumber && (
+        <div className={styles.tableTabsLastUpdatedBlock}>Last updated block {blockNumber}</div>
+      )}
+    </div>
+  )
+}
+
+function RankTable (props: any) {
   const { title, subtitle, gasData } = props
   return (
     <div className={styles.gasTable}>
@@ -90,218 +336,6 @@ const GasTable = (props: any) => {
           </tbody>
         </table>
       </div>
-    </div>
-  )
-}
-
-function useDate() {
-  const { isLoading, data, error } = useQuery(['date'], async () => {
-    const dt = DateTime.utc()
-    const formattedDate = dt.toFormat('ccc, dd LLL yyyy HH:mm:ss \'UTC\'')
-    return formattedDate
-  }, {
-    enabled: true,
-    refetchInterval: 1 * 1000
-  })
-
-  return {
-    date: data ?? ''
-  }
-}
-
-function useRelativeDate(timestamp: number) {
-  const { isLoading, data, error } = useQuery([`relativedate:${timestamp}`, timestamp], async () => {
-    if (!timestamp) {
-      return ''
-    }
-    return DateTime.fromSeconds(timestamp).toRelative()
-  }, {
-    enabled: true,
-    refetchInterval: 1 * 1000
-  })
-
-  return {
-    date: data ?? ''
-  }
-}
-
-function useGasEstimates() {
-  const { isLoading, data, error } = useQuery(['gasEstimates'], async () => {
-    return fetch(apiUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        method: 'gasTracker_getGasEstimates',
-        params: [],
-        id: 1
-      })
-    })
-    .then(response => response.json())
-    .then((data: any) => data.result.estimates)
-  }, {
-    enabled: true,
-    refetchInterval: refreshInterval
-  })
-
-  return {
-    estimates: data ?? []
-  }
-}
-
-function useTimeRange(defaultValue: string) {
-  const [timeRange, setTimeRange] = useState(defaultValue)
-  return {
-    timeRange,
-    setTimeRange
-  }
-}
-
-function useGasPrices(timeRange: string) {
-  const { isLoading, data, error } = useQuery(['gasPrices', timeRange], async () => {
-    return fetch(apiUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        method: 'gasTracker_getHistoricalGasPrices',
-        params: [timeRange],
-        id: 1
-      })
-    })
-    .then(response => response.json())
-    .then((data: any) => data.result.gasPrices)
-  }, {
-    enabled: true,
-    refetchInterval: refreshInterval
-  })
-
-  return {
-    gasPrices: data ?? []
-  }
-}
-
-function useTopGasSpenders (timeRange: string) {
-  const { isLoading, data, error } = useQuery(['topGasSpenders', [timeRange]], async () => {
-    return fetch(apiUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        method: 'gasTracker_getTopGasSpenders',
-        params: [timeRange],
-        id: 1
-      })
-    })
-    .then(response => response.json())
-    .then((data: any) => data.result.gasSpenders)
-  }, {
-    enabled: true,
-    refetchInterval: refreshInterval
-  })
-
-  return {
-    topGasSpenders: data ?? []
-  }
-}
-
-function useTopGasGuzzlers(timeRange: string) {
-  const { isLoading, data, error } = useQuery(['topGasGuzzlers', timeRange], async () => {
-    return fetch(apiUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        method: 'gasTracker_getTopGasGuzzlers',
-        params: [timeRange],
-        id: 1
-      })
-    })
-    .then(response => response.json())
-    .then((data: any) => data.result.gasGuzzlers)
-  }, {
-    enabled: true,
-    refetchInterval: refreshInterval
-  })
-
-  return {
-    topGasGuzzlers: data ?? []
-  }
-}
-
-function useGetCurrentEthUsdPrice() {
-  const { isLoading, data, error } = useQuery(['currentEthUsdPrice'], async () => {
-    return fetch(apiUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        method: 'gasTracker_getCurrentEthUsdPrice',
-        params: [],
-        id: 1
-      })
-    })
-    .then(response => response.json())
-    .then((data: any) => data.result)
-  }, {
-    enabled: true,
-    refetchInterval: refreshInterval
-  })
-
-  return {
-    price: data?.price,
-    priceDisplay: data?.priceDisplay,
-    timestamp: data?.timestamp,
-  }
-}
-
-function useGetCurrentGasPrice() {
-  const { isLoading, data, error } = useQuery(['currentGasPrice'], async () => {
-    return fetch(apiUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        method: 'gasTracker_getCurrentGasPrice',
-        params: [],
-        id: 1
-      })
-    })
-    .then(response => response.json())
-    .then((data: any) => data.result)
-  }, {
-    enabled: true,
-    refetchInterval: refreshInterval
-  })
-
-  return {
-    gasPrice: data?.gasPrice,
-    timestamp: data?.timestamp,
-  }
-}
-
-function Header () {
-  return (
-    <div className={styles.titleContainer}>
-      <a href={"/"}><Image src="/assets/optimism.svg" alt="" width={200} height={40} /> <span className={styles.title}>Gas Tracker ⛽</span></a>
-    </div>
-  )
-}
-
-function SubHeader (props: any) {
-  const { blockInfo } = props
-  const { date } = useDate()
-  return (
-    <div className={styles.subTitleContainer}>
-      {/*<Countdown initialCount={refreshInterval / 1000} />*/}
-      <BlockInfo blockNumber={blockInfo?.blockNumber} gasPrice={blockInfo?.gasPrice} />
-      <div title="Current date and time in UTC">{date}</div>
     </div>
   )
 }
@@ -388,62 +422,6 @@ function PriceBoxes (props: any) {
   )
 }
 
-function TimeRange (props: any) {
-  const { onChange, timeRange } = props
-  let readableTimeRange = ''
-  if (timeRange === '10m') {
-    readableTimeRange = '10 minutes'
-  }
-  if (timeRange === '1h') {
-    readableTimeRange = 'hour'
-  }
-  if (timeRange === '24h') {
-    readableTimeRange = '24 hours'
-  }
-  if (timeRange === '7d') {
-    readableTimeRange = '7 days'
-  }
-  return (
-    <div className={styles.timeRangeContainer}>
-      <div title="Time range for data shown">Last {readableTimeRange}</div>
-      <div className={styles.timeRange}>
-        <button title="10 minutes" className={timeRange === '10m' ? styles.timeRangeButtonSelected : styles.timeRangeButton} onClick={() => onChange('10m')}>10m</button>
-        <button title="1 hour" className={timeRange === '1h' ? styles.timeRangeButtonSelected : styles.timeRangeButton} onClick={() => onChange('1h')}>1H</button>
-        <button disabled title="24 hours (NOT ENOUGH DATA YET)" className={timeRange === '24h' ? styles.timeRangeButtonSelected : styles.timeRangeButton} onClick={() => onChange('24h')}>24H</button>
-        <button disabled title="7 days (NOT ENOUGH DATA YET)" className={timeRange === '7d' ? styles.timeRangeButtonSelected : styles.timeRangeButton} onClick={() => onChange('7d')}>7D</button>
-      </div>
-    </div>
-  )
-}
-
-function TableTab(props: any) {
-  const { title, subtitle, blockNumber, selected } = props
-  return (
-    <div className={selected ? styles.tableTabSelected : styles.tableTab}>
-      <h2 className={styles.tableTabTitle}>⛽ {title}</h2>
-      <div className={styles.tableTabSubtitle}>{subtitle}</div>
-      <div className={styles.tableTabsLastUpdatedBlock}>Last updated block {blockNumber}</div>
-    </div>
-  )
-}
-
-
-function BlockInfo (props: any) {
-  const { blockNumber, gasPrice } = props
-  return (
-    <div className={styles.blockInfo}>
-      {!!gasPrice?.gwei && (
-        <div className={styles.blockInfoGasPrice} title="Latest gas price denominated in Gwei">Gas Price: {gasPrice?.gwei} gwei</div>
-      )}
-      {!!blockNumber && (
-        <div className={styles.blockInfoBlock} title="Block number of gas price shown">
-          <a href={`https://optimistic.etherscan.io/block/${blockNumber}`} target="_blank">Last updated block {blockNumber}</a>
-        </div>
-      )}
-    </div>
-  )
-}
-
 function Footer () {
   return (
       <footer className={styles.footer}>
@@ -457,16 +435,6 @@ function Footer () {
   )
 }
 
-function Banner () {
-  return (
-    <div className={styles.banner}>
-      <div className={styles.innerBanner}>
-        {'⚠️ This project is still in development.'}
-      </div>
-    </div>
-  )
-}
-
 function Main() {
   const { timeRange, setTimeRange } = useTimeRange('10m')
   const { timeRange: tableTimeRange, setTimeRange: setTableTimeRange } = useTimeRange('1h')
@@ -477,8 +445,6 @@ function Main() {
   const { topGasGuzzlers } = useTopGasGuzzlers(tableTimeRange)
   const [activeTab, setActiveTab] = useState('tab1')
   const blockInfo = gasPrices?.[gasPrices?.length - 1]
-  const topGasGuzzlersBlockNumber = blockInfo?.blockNumber
-  const topGasSpendersBlockNumber = blockInfo?.blockNumber
 
   return (
     <main className={styles.main}>
@@ -491,25 +457,16 @@ function Main() {
       <LineChart timeRange={timeRange} gasPrices={gasPrices} />
       <PriceBoxes priceDisplay={priceDisplay} gasPrice={gasPrice} priceTimestamp={priceTimestamp} gasPriceTimestamp={gasPriceTimestamp} />
       <GasEstimates />
-      <div className={styles.tableTabsContainer}>
-        <div className={styles.tableTabs}>
-          <div onClick={event => setActiveTab('tab1')}>
-            <TableTab selected={activeTab === 'tab1'} title="Top Gas Guzzlers" subtitle={"Sending Accounts that pay a lot of Gas"} blockNumber={topGasGuzzlersBlockNumber} />
-          </div>
-          <div onClick={event => setActiveTab('tab2')}>
-            <TableTab selected={activeTab === 'tab2'} title="Top Gas Spenders" subtitle={"Contracts / Accounts that consume a lot of Gas"} blockNumber={topGasSpendersBlockNumber} />
-          </div>
-        </div>
-      </div>
+      <TableTabs activeTab={activeTab} setActiveTab={setActiveTab} />
       <div style={{ width: '100%', 'maxWidth': '740px', 'marginBottom': '0.5rem' }}>
         <TimeRange onChange={setTableTimeRange} timeRange={tableTimeRange} />
       </div>
       <div className={styles.gasTableContainer}>
         {activeTab === 'tab1' && (
-          <GasTable gasData={topGasGuzzlers} />
+          <RankTable gasData={topGasGuzzlers} />
         )}
         {activeTab === 'tab2' && (
-          <GasTable gasData={topGasSpenders} />
+          <RankTable gasData={topGasSpenders} />
         )}
       </div>
       <div className={styles.infoFooter}>
