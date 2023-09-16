7356f8a3a1a9da5cf0ae00f3a93e2451add7a724
buildooor <buildooor@protonmail.com>
2023-09-16 03:29:49 -0700

    backend: Update caching

--- packages/backend/src/middleware/responseCache.ts
+++ packages/backend/src/middleware/responseCache.ts
@@ -1,11 +1,19 @@
 import mcache from 'memory-cache'
 
-const durationMs = 5 * 1000
-
 export function responseCache (req: any, res: any, next: any) {
+  let durationMs = 10 * 1000
   const urlKey = req.originalUrl || req.url
   const paramsKey = req.body ? JSON.stringify(req.body) : ''
   const key = `__express__${urlKey}::${paramsKey}}`
+  if (key.includes('"1h"')) {
+    durationMs = 5 * 60 * 1000
+  }
+  if (key.includes('"7d"')) {
+    durationMs = 60 * 60 * 1000
+  }
+  if (key.includes('"24h"')) {
+    durationMs = 10 * 60 * 1000
+  }
   const cachedBody = mcache.get(key)
   if (cachedBody) {
     res.send(cachedBody)
